67ea280c58fcb5aa3b1058c31d5511cd3a04415c
Ben Stein <whereyouben@gmail.com>
2021-05-05 18:10:35 -0400

    added skills to data and render

--- src/components/CharacterSheet.js
+++ src/components/CharacterSheet.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { data } from '../fakeDb/fakeData';
 
 function CharacterSheet() {
+  const bscSkillKeys = Object.keys(data.basicSkills)
+  const advSkillKeys = Object.keys(data.advSkills)
+  function totalSkill({stat, adv}) {
+    const skill = data.stats[stat] + adv;
+    return skill;
+  }
 
   return(
     <div>
+      {/* Refactor out into details component */}
       <div className="sheet-row">
         <div className="sheet-div">
           {data.name}
@@ -19,36 +26,60 @@ function CharacterSheet() {
           {data.career}
         </div>
       </div>
+
+      {/* Refactor out into stats component */}
       <div className="sheet-row">
         <div className="sheet-div">
-          WS: {data.characteristics.WS}
+          WS: {data.stats.WS}
         </div>
         <div className="sheet-div">
-          DS: {data.characteristics.DS}
+          DS: {data.stats.DS}
         </div>
         <div className="sheet-div">
-          S: {data.characteristics.S}
+          S: {data.stats.S}
         </div>
         <div className="sheet-div">
-          T: {data.characteristics.T}
+          T: {data.stats.T}
         </div>
         <div className="sheet-div">
-          I: {data.characteristics.I}
+          I: {data.stats.I}
         </div>
         <div className="sheet-div">
-          Ag: {data.characteristics.Ag}
+          Ag: {data.stats.Ag}
         </div>
         <div className="sheet-div">
-          Dex: {data.characteristics.Dex}
+          Dex: {data.stats.Dex}
         </div>
         <div className="sheet-div">
-          Int: {data.characteristics.Int}
+          Int: {data.stats.Int}
         </div>
         <div className="sheet-div">
-          WP: {data.characteristics.WP}
+          WP: {data.stats.WP}
         </div>
         <div className="sheet-div">
-          Fel: {data.characteristics.Fel}
+          Fel: {data.stats.Fel}
+        </div>
+      </div>
+
+      {/* Refactor out into skill tables component */}
+      <div className="sheet-row">
+        <div className="sheet-div">
+          Basic Skills
+          <div className="sheet-div">
+            {bscSkillKeys[0]}: {totalSkill(data.basicSkills["Melee (Basic)"])}
+          </div>
+          <div className="sheet-div">
+            {bscSkillKeys[1]}: {totalSkill(data.basicSkills["Art"])}
+          </div>
+        </div>
+        <div className="sheet-div">
+          Advanced Skills
+          <div className="sheet-div">
+            {advSkillKeys[0]}: {totalSkill(data.advSkills["Lore (Witches)"])}
+          </div>
+          <div className="sheet-div">
+            {advSkillKeys[1]}: {totalSkill(data.advSkills["Melee (Polearm)"])}
+          </div>
         </div>
       </div>
     </div>
--- src/fakeDb/fakeData.js
+++ src/fakeDb/fakeData.js
@@ -3,16 +3,36 @@ export const data = {
   species: "test-species",
   class: "test-class",
   career: "test-career",
-  characteristics: {
-    WS: 10,
-    BS: 20,
-    S: 30,
-    T: 40,
-    I: 50,
-    Ag: 60,
-    Dex: 70,
-    Int: 80,
-    WP: 90,
-    Fel: 100
+  stats: {
+    "WS": 10,
+    "BS": 20,
+    "S": 30,
+    "T": 40,
+    "I": 50,
+    "Ag": 60,
+    "Dex": 70,
+    "Int": 80,
+    "WP": 90,
+    "Fel": 100
   },
+  basicSkills: {
+    "Melee (Basic)": {
+      stat: "WS",
+      adv: 10
+    },
+    "Art": {
+      stat: "Dex",
+      adv: 5
+    }
+  },
+  advSkills: {
+    "Melee (Polearm)": {
+      stat: "WS",
+      adv: 7
+    },
+    "Lore (Witches)": {
+      stat: "Int",
+      adv: 10
+    }
+  }
 }
