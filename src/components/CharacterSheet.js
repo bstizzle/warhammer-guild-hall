b2091691870f497182fcd2178a0e16b932b7cb00
Ben Stein <whereyouben@gmail.com>
2021-05-05 14:26:49 -0400

    first commit

---
+++ src/components/CharacterSheet.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function CharacterSheet() {
+
+  return(
+    <div>
+      <div className="sheet-row">
+        <div className="sheet-div">
+          Hello
+        </div>
+        <div className="sheet-div">
+          there
+        </div>
+        <div className="sheet-div">
+          i'm
+        </div>
+        <div className="sheet-div">
+          a
+        </div>
+        <div className="sheet-div">
+          character
+        </div>
+      </div>
+      <div className="sheet-row">
+        <div className="sheet-div">
+          here's
+        </div>
+        <div className="sheet-div">
+          my
+        </div>
+        <div className="sheet-div">
+          sheet
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CharacterSheet;
