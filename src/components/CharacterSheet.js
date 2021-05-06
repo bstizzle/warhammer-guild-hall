1f3b436394dc36a2f9e77eb009fd2d0bce1e1b68
Ben Stein <whereyouben@gmail.com>
2021-05-06 12:10:38 -0400

    refactored stats component

--- src/components/CharacterSheet.js
+++ src/components/CharacterSheet.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { data } from '../fakeDb/fakeData';
-
+import Stats from './Stats';
 import { useSelector, useDispatch } from 'react-redux';
 import { setChar, selectChar } from '../redux/characterSlice';
 
@@ -13,23 +13,19 @@ const CharacterSheet = () => {
     dispatch(setChar(data))
   }, [dispatch])
 
-  // function charDispatch(){
-  //   dispatch(setChar(data))
-  // }
-
   function totalSkill({stat, adv}) {
     const skill = data.stats[stat] + adv;
     return skill;
   }
 
-  let bscSkillKeys = Object.keys(data.basicSkills);
-  let advSkillKeys = Object.keys(data.advSkills);
-  // let bscSkillKeys;
-  // let advSkillKeys;
-  // if(char !== null) {
-  //   bscSkillKeys = Object.keys(char.basicSkills)
-  //   advSkillKeys = Object.keys(char.advSkills)
-  // }
+  // let bscSkillKeys = Object.keys(data.basicSkills);
+  // let advSkillKeys = Object.keys(data.advSkills);
+  let bscSkillKeys;
+  let advSkillKeys;
+  if(char.basicSkills) {
+    bscSkillKeys = Object.keys(char.basicSkills)
+    advSkillKeys = Object.keys(char.advSkills)
+  }
 
   return(
     <div>
@@ -51,58 +47,30 @@ const CharacterSheet = () => {
         </div>
       </div>
 
-      {/* Refactor out into stats component */}
-      <div className="sheet-row">
-        <div className="sheet-div">
-          WS: {data.stats.WS}
-        </div>
-        <div className="sheet-div">
-          BS: {data.stats.BS}
-        </div>
-        <div className="sheet-div">
-          S: {data.stats.S}
-        </div>
-        <div className="sheet-div">
-          T: {data.stats.T}
-        </div>
-        <div className="sheet-div">
-          I: {data.stats.I}
-        </div>
-        <div className="sheet-div">
-          Ag: {data.stats.Ag}
-        </div>
-        <div className="sheet-div">
-          Dex: {data.stats.Dex}
-        </div>
-        <div className="sheet-div">
-          Int: {data.stats.Int}
-        </div>
-        <div className="sheet-div">
-          WP: {data.stats.WP}
-        </div>
-        <div className="sheet-div">
-          Fel: {data.stats.Fel}
-        </div>
-      </div>
+      {char.stats ?
+        <Stats />
+        :
+        null
+      }
 
       {/* Refactor out into skill tables component */}
       <div className="sheet-row">
         <div className="sheet-div">
           Basic Skills
           <div className="sheet-div">
-            {bscSkillKeys[0]}: {totalSkill(data.basicSkills["Melee (Basic)"])}
+            {char.basicSkills ? bscSkillKeys[0] : null}: {totalSkill(data.basicSkills["Melee (Basic)"])}
           </div>
           <div className="sheet-div">
-            {bscSkillKeys[1]}: {totalSkill(data.basicSkills["Art"])}
+            {char.basicSkills ? bscSkillKeys[1] : null}: {totalSkill(data.basicSkills["Art"])}
           </div>
         </div>
         <div className="sheet-div">
           Advanced Skills
           <div className="sheet-div">
-            {advSkillKeys[1]}: {totalSkill(data.advSkills["Lore (Witches)"])}
+            {char.advSkills ? advSkillKeys[1] : null}: {totalSkill(data.advSkills["Lore (Witches)"])}
           </div>
           <div className="sheet-div">
-            {advSkillKeys[0]}: {totalSkill(data.advSkills["Melee (Polearm)"])}
+            {char.advSkills ? advSkillKeys[0] : null}: {totalSkill(data.advSkills["Melee (Polearm)"])}
           </div>
         </div>
       </div>
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+import { selectChar } from '../redux/characterSlice';
+
 function Stats() {
+  const char = useSelector(selectChar)
+
+  function statJSX(stat){
+    return(
+      <div className="sheet-div">
+        {stat}: {char.stats[stat]}
+      </div>
+    )
+  }
 
   return(
-
+    <div className="sheet-row">
+      {statJSX("WS")}
+      {statJSX("BS")}
+      {statJSX("S")}
+      {statJSX("T")}
+      {statJSX("I")}
+      {statJSX("Ag")}
+      {statJSX("Dex")}
+      {statJSX("Int")}
+      {statJSX("WP")}
+      {statJSX("Fel")}
+    </div>
   );
 }
 
