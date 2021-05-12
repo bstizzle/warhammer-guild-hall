0b4d557c160b0bc06430c35e944a590a29c93d31
Ben Stein <whereyouben@gmail.com>
2021-05-12 18:01:49 -0400

    first commit and installed fomantic-ui

--- src/App.js
+++ src/App.js
@@ -4,33 +4,17 @@ import CharacterSheet from './components/CharacterSheet';
 import Header from './components/Header';
 import Home from './components/Home';
 
-import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from './styles/theme';
-import { GlobalStyles } from './styles/global';
-
 function App() {
-  const [theme, setTheme] = useState('dark');
-  const toggleTheme = () => {
-    if(theme === 'light') {
-      setTheme('dark');
-    } else {
-      setTheme('light');
-    }
-  }
 
   return (
     <div className="app-container">
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <GlobalStyles />
       <Header />
-      <button onClick={toggleTheme}>Toggle Theme</button>
       <Route exact path='/'>
         <Home />
       </Route>
       <Route path='/sheet'>
         <CharacterSheet />
       </Route>
-    </ThemeProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-// import './index.css';
+import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
