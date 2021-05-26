2ec89175af1ab78928c152a9c88bf6eff2ff34c8
Ben Stein <whereyouben@gmail.com>
2021-05-26 12:19:50 -0400

    adv skill names update and save

--- src/components/table-components/cellTypeLogic.js
+++ src/components/table-components/cellTypeLogic.js
@@ -3,7 +3,7 @@ export const isBscSkillCell = (char, setChar, record, field) => {
   const index = record.key
   const numField = parseInt(field, 10)
   //makes sure we don't overwrite the adv value when total updates
-  if(numField !== record.total){
+  if(numField !== record.total) {
     const updatedBasics = char.basicSkills
     updatedBasics.splice(index, 1, {
       name: record.name,
@@ -21,8 +21,19 @@ export const isBscSkillCell = (char, setChar, record, field) => {
 export const isAdvSkillCell = (char, setChar, record, field) => {
   const index = record.key 
   const numField = parseInt(field, 10)
-  
-  if(numField !== record.total){
+  if(isNaN(numField)){
+    const updatedNames = char.advSkills
+    updatedNames.splice(index, 1, {
+      name: field,
+      stat: record.stat,
+      adv: record.adv
+    })
+    console.log('is adv skill name!')
+    setChar(char => ({
+      ...char,
+      advSkills: updatedNames
+    }))
+  } else if(numField !== record.total) {
     const updatedAdvs = char.advSkills
     updatedAdvs.splice(index, 1, {
       name: record.name,
@@ -41,7 +52,7 @@ export const isTalentCell = (char, setChar, record, field) => {
   const index = record.key
   const updatedTalents = char.talents
   //check whether we are updating the name or the times
-  if(Number.isInteger(parseInt(field, 10))){
+  if(Number.isInteger(parseInt(field, 10))) {
     console.log('is number')
     updatedTalents.splice(index, 1, {
       name: record.name,
